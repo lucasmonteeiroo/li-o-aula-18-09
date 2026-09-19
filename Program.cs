602ea731208193f66e5891b284e9406afe396c87
lucas augusto <luaugustomonteiror@gmail.com>
2026-09-18 21:37:39 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+double num1, num2, num3, media;
+
+
+Console.WriteLine("--- Média Aritmética de 3 números ---");
+Console.WriteLine();
+
+
+Console.Write("Digite o primeiro número: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite o segundo número: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite o terceiro número: ");
+num3 = Convert.ToDouble(Console.ReadLine());
+
+
+media = num1 + num2 + num3 / 3;
+
+
+Console.WriteLine($"\nMédia: {media:N1}");
